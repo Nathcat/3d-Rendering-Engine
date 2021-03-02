8706a0a12c669eaf2ec913143727b5f6c2ddbb47
Nathcat <supernath126@gmail.com>
2021-03-02 08:55:17 +0000

    Updated example.

--- test.py
+++ test.py
@@ -1,13 +1,29 @@
-
 from Vertex import *
 from RenderingEngine import *
 
-
 vertices = [Vertex((-1, -1, -1)),
+            Vertex((-1, 1, -1)),
+            Vertex((-1, -1, -1)),
+
             Vertex((1, -1, -1)),
+            Vertex((1, 1, -1)),
+            Vertex((1, -1, -1)),
+
             Vertex((1, -1, 1)),
+            Vertex((1, 1, 1)),
+            Vertex((1, -1, 1)),
+
             Vertex((-1, -1, 1)),
-            Vertex((-1, -1, -1)),]
+            Vertex((-1, 1, 1)),
+            Vertex((-1, -1, 1)),
+
+            Vertex((-1, -1, -1)),
+            Vertex((-1, 1, -1)),
+
+            Vertex((1, 1, -1)),
+            Vertex((1, 1, 1)),
+            Vertex((-1, 1, 1)),
+            Vertex((-1, 1, -1)),]
 
 engine = Engine(vertices, (0, 0, 5))
 
